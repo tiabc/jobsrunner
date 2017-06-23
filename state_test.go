de657d1af5de191e255e634a4a32183478bbdcc6
Ivan Tomilov <tiabc.work@gmail.com>
2017-06-23 23:27:12 +0700

    Wrote test for TestNewFromFile

---
+++ state_test.go
@@ -0,0 +1,14 @@
+package jobrunner
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNewFromFile(t *testing.T) {
+	// Arrange.
+	_, err := NewFromFile("testdata/config.json")
+
+	// Assert.
+	require.Nil(t, err)
+}
