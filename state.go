4ce9138817e12cd7e6494dc7b91766ffcfbe08ba
Ivan Tomilov <tiabc.work@gmail.com>
2017-06-25 10:09:19 +0700

    Renamed State to Runtime

--- state.go
+++ state.go
@@ -8,33 +8,33 @@ import (
 	"time"
 )
 
-// State of jobrunner.
-type State struct {
+// Runtime of jobrunner.
+type Runtime struct {
 	Conf Config
 	//Logger log.Logger
 
 	ctx context.Context
 }
 
-// NewFromFile creates State with Config parsed from the specified file.
-func NewFromFile(filename string) (State, error) {
+// NewFromFile creates Runtime with Config parsed from the specified file.
+func NewFromFile(filename string) (Runtime, error) {
 	conf, err := NewConfigFromFile(filename)
 	if err != nil {
-		return State{}, fmt.Errorf("can't create config: %s", err)
+		return Runtime{}, fmt.Errorf("can't create config: %s", err)
 	}
-	return State{
+	return Runtime{
 		Conf: conf,
 	}, nil
 }
 
 // Run the configured jobs.
-func (s *State) Run(ctx context.Context) {
+func (r *Runtime) Run(ctx context.Context) {
 	var wg sync.WaitGroup
-	s.ctx = ctx
-	for i, job := range s.Conf.Jobs {
+	r.ctx = ctx
+	for i, job := range r.Conf.Jobs {
 		wg.Add(1)
 		go func() {
-			s.startJob(job)
+			r.startJob(job)
 			// TODO: Logger dependency.
 			log.Printf("Job #%d finished", i)
 			wg.Done()
@@ -43,13 +43,13 @@ func (s *State) Run(ctx context.Context) {
 	wg.Wait()
 }
 
-func (s *State) startJob(job ConfigJob) {
+func (r *Runtime) startJob(job ConfigJob) {
 	// TODO: Run CMD and output the response in case of a non-zero exit code.
 	// TODO: Logger dependency.
 	log.Println(job.Cmd)
 	for {
 		select {
-		case <-s.ctx.Done():
+		case <-r.ctx.Done():
 			return
 		case <-time.Tick(time.Duration(job.Interval)):
 			// TODO: Run CMD and output the response in case of a non-zero exit code.
